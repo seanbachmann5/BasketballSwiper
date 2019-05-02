5a9b5a2f706ef53fcefb9547faf12815e24cff16
LUKE REUPERT <luke.reupert19@clcmbp30.local>
2019-05-02 12:56:03 -0500

    Added a swipe action

--- BasketballSwiper/GameScene.swift
+++ BasketballSwiper/GameScene.swift
@@ -10,8 +10,46 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        
+        var swipeRight = UISwipeGestureRecognizer(target: self, action: "respondToSwipeGesture:")
+        swipeRight.direction = UISwipeGestureRecognizer.Direction.right
+        self.view?.addGestureRecognizer(swipeRight)
+        
+        var swipeDown = UISwipeGestureRecognizer(target: self, action: "respondToSwipeGesture:")
+        swipeDown.direction = UISwipeGestureRecognizer.Direction.down
+        self.view?.addGestureRecognizer(swipeDown)
+        
+        
+    }
     
-   
+    func respondToSwipeGesture(gesture: UIGestureRecognizer) {
+        
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            
+            
+            switch swipeGesture.direction {
+            case UISwipeGestureRecognizer.Direction.right:
+                print("Swiped right")
+            case UISwipeGestureRecognizer.Direction.down:
+                print("Swiped down")
+            case UISwipeGestureRecognizer.Direction.left:
+                print("Swiped left")
+            case UISwipeGestureRecognizer.Direction.up:
+                print("Swiped up")
+            default:
+                break
+                
+            }
+        }
+    }
+    
+    
     
     
 }
+
+   
+    
+    
+
