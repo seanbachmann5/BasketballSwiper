c10625cbf41b1a93fcf735b3ff016364ecf3d60b
Benjamin Novotney <benjamin.novotney20@clcmbp14.local>
2019-05-06 13:14:32 -0500

    Deleted the gesture stuff that didn't work

--- BasketballSwiper/GameScene.swift
+++ BasketballSwiper/GameScene.swift
@@ -13,41 +13,9 @@ class GameScene: SKScene {
     
     var holding:Timer!
     
-    
-    
     override func didMove(to view: SKView) {
-        
-        var swipeRight = UISwipeGestureRecognizer(target: self, action: "respondToSwipeGesture:")
-        swipeRight.direction = UISwipeGestureRecognizer.Direction.right
-        self.view?.addGestureRecognizer(swipeRight)
-        
-        var swipeDown = UISwipeGestureRecognizer(target: self, action: "respondToSwipeGesture:")
-        swipeDown.direction = UISwipeGestureRecognizer.Direction.down
-        self.view?.addGestureRecognizer(swipeDown)
-        
-        
     }
     
-    func respondToSwipeGesture(gesture: UIGestureRecognizer) {
-        
-        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
-            
-            
-            switch swipeGesture.direction {
-            case UISwipeGestureRecognizer.Direction.right:
-                print("Swiped right")
-            case UISwipeGestureRecognizer.Direction.down:
-                print("Swiped down")
-            case UISwipeGestureRecognizer.Direction.left:
-                print("Swiped left")
-            case UISwipeGestureRecognizer.Direction.up:
-                print("Swiped up")
-            default:
-                break
-                
-            }
-        }
-    }
     @objc func runGame(){
     
     
@@ -57,9 +25,10 @@ class GameScene: SKScene {
     
     
     
-}
+
 
    
     
     
 
+}
