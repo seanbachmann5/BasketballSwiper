fcb1f8562a160634043ec5d3cdbf376e6a68a843
LUKE REUPERT <luke.reupert19@clcmbp30.local>
2019-05-06 13:00:46 -0500

    Wiped out game scene swift

--- BasketballSwiper/GameScene.swift
+++ BasketballSwiper/GameScene.swift
@@ -16,38 +16,7 @@ class GameScene: SKScene, UIGestureRecognizerDelegate {
     override func didMove(to view: SKView) {
         
         
-        
-        var swipeRight = UISwipeGestureRecognizer(target: self, action: Selector(("respondToSwipeGesture:")))
-        swipeRight.direction = UISwipeGestureRecognizer.Direction.right
-        self.view?.addGestureRecognizer(swipeRight)
-        
-        var swipeDown = UISwipeGestureRecognizer(target: self, action: Selector(("respondToSwipeGesture:")))
-        swipeDown.direction = UISwipeGestureRecognizer.Direction.down
-        self.view?.addGestureRecognizer(swipeDown)
-        
-        
-    }
-    
-    func respondToSwipeGesture(gesture: UIGestureRecognizer) {
-        
-        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
-            
-            
-            switch swipeGesture.direction {
-            case UISwipeGestureRecognizer.Direction.right:
-                print("Swiped right")
-            case UISwipeGestureRecognizer.Direction.down:
-                print("Swiped down")
-            case UISwipeGestureRecognizer.Direction.left:
-                print("Swiped left")
-            case UISwipeGestureRecognizer.Direction.up:
-                print("Swiped up")
-            default:
-                break
-                
-            }
-        }
-    }
+       
     
     
     
@@ -58,3 +27,4 @@ class GameScene: SKScene, UIGestureRecognizerDelegate {
     
     
 
+}
