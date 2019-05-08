4d35d75a31ec098ad778f522ccdfdf77729a5760
Benjamin Novotney <benjamin.novotney20@clcmbp14.local>
2019-05-08 12:53:39 -0500

    Ninja Hiya

--- BasketballSwiper/GameScene.swift
+++ BasketballSwiper/GameScene.swift
@@ -8,44 +8,68 @@
 
 import SpriteKit
 import GameplayKit
+struct physicsCategory {
+     static let player: UInt32 = 1
+    static let basketballHoop: UInt32 = 2
+
+}
+
 var player = SKSpriteNode()
+var basketballHoop = SKSpriteNode()
 
-
-class GameScene: SKScene {
+class GameScene: SKScene,SKPhysicsContactDelegate  {
 override func didMove(to view: SKView) {
-       
+    physicsWorld.contactDelegate = self
+
     self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
         player.anchorPoint = CGPoint(x: 0.5, y: 0.5)
         player = SKSpriteNode(imageNamed: "Basketball")
  player.position = CGPoint(x: 0, y: 0)
   player.scale(to: CGSize(width: 75, height: 75))
-  //trying to commit
+  player.zPosition = 1
+    //trying to commit
     let border = SKPhysicsBody(edgeLoopFrom: self.frame)
     self.physicsBody = border
-    self.addChild(player)
+  basketballHoop.physicsBody?.contactTestBitMask = physicsCategory.basketballHoop
+    player.physicsBody?.categoryBitMask = physicsCategory.player
     player.physicsBody?.affectedByGravity = true
-    
-   self.addChild(player)
+    self.addChild(player)
     }
-    
- 
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches{
         let location = touch.location(in: self)
             player.position.x = location.x
             player.position.y = location.y
-            
-        
     }
-      
+        func createBasketballHoop() {
+            let basketballHoop = SKSpriteNode(imageNamed: "Basketball Hoop")
+            //enemy.position = randomPoint()
+           // basketballHoop.physicsBody = SKPhysicsBody(rectangleOf: basketballHoop.size)
+           basketballHoop.physicsBody?.mass = 1
+            basketballHoop.scale(to: CGSize(width: 50, height: 50))
+           basketballHoop.physicsBody?.categoryBitMask = physicsCategory.basketballHoop
+            basketballHoop.physicsBody?.contactTestBitMask = physicsCategory.player
+            //enemy.physicsBody?.collisionBitMask = PhysicsCategory.none
+            basketballHoop.physicsBody?.isDynamic = true
+            addChild(basketballHoop)
+            
+            let actionMove = SKAction.move(to: CGPoint(x: 0, y: 300), duration: TimeInterval(CGFloat.random(in: 2.0...4.0)))
+            let actionMoveDone = SKAction.removeFromParent()
+           basketballHoop.run(SKAction.sequence([actionMove,actionMoveDone]))
          
 
     
     
 }
     
-    
-    
+        func didBegin(_ contact: SKPhysicsContact) {
+            print("contact")
+            let one = contact.bodyA.node as? SKSpriteNode
+            let two = contact.bodyB.node as? SKSpriteNode
+            one?.removeFromParent()
+            two?.removeFromParent()
+       
+        }
 
 
    
@@ -54,4 +78,6 @@ override func didMove(to view: SKView) {
 
 
 
+
+}
 }
