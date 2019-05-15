5a92456c021630e78ee06b2f8f4ffbd1e605e891
Benjamin Novotney <benjamin.novotney20@clcmbp14.local>
2019-05-15 13:09:49 -0500

    bounce add score! - cam

--- BasketballSwiper/GameScene.swift
+++ BasketballSwiper/GameScene.swift
@@ -18,51 +18,48 @@ var player = SKSpriteNode()
 var basketballHoop = SKSpriteNode()
 var score = 0
 var myScoreLabel:SKLabelNode!
+var hit = 0
 
 class GameScene: SKScene,SKPhysicsContactDelegate  {
     
     func scoreCount() {
-        score += 2
+        hit += 1
+        print("score count was called \(hit)")
+        score += 1
             if let myScoreLabel = self.childNode(withName: "Score") as? SKLabelNode{
-           
-                myScoreLabel.text = "Score  \(score)"
+            myScoreLabel.text = "Score  \(score)"
         }
     }
    
     override func didMove(to view: SKView) {
-        let border = SKPhysicsBody(edgeLoopFrom: self.frame)
-        self.physicsBody = border
-        createBasketballHoop()
-        scoreCount()
-        print("score Added")
         physicsWorld.contactDelegate = self
-    self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        let border = SKPhysicsBody(edgeLoopFrom: self.frame)
+        self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        self.physicsBody = border
+        
+        createBasketballHoop()
+        
         player.anchorPoint = CGPoint(x: 0.5, y: 0.5)
         player = SKSpriteNode(imageNamed: "Basketball")
         player.position = CGPoint(x: 0, y: 0)
         player.scale(to: CGSize(width: 75, height: 75))
         player.zPosition = 1
-      
-    //trying to commit
-        
-
-        
         player.physicsBody = SKPhysicsBody(circleOfRadius: player.size.width / 2)
-        player.physicsBody?.contactTestBitMask = physicsCategory.basketballHoop
-        player.physicsBody?.categoryBitMask = physicsCategory.player
+        player.physicsBody?.contactTestBitMask = 2
+        player.physicsBody?.categoryBitMask = 1
         player.physicsBody?.affectedByGravity = true
         player.physicsBody?.mass = 0.1
-self.addChild(player)
-   
+        self.addChild(player)
     }
+    
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches{
         let location = touch.location(in: self)
             player.position.x = location.x
             player.position.y = location.y
         }
-            
-        }
+    }
+    
         func createBasketballHoop() {
             let basketballHoop = SKSpriteNode(imageNamed: "Basketball Hoop")
             //enemy.position = randomPoint()
@@ -70,36 +67,17 @@ self.addChild(player)
             basketballHoop.physicsBody?.mass = 1
             basketballHoop.scale(to: CGSize(width: 653, height: 496))
             basketballHoop.position = CGPoint(x: -0, y: 419)
-            basketballHoop.physicsBody?.categoryBitMask = physicsCategory.basketballHoop
-            basketballHoop.physicsBody?.contactTestBitMask = physicsCategory.player
+            basketballHoop.physicsBody?.categoryBitMask = 2
             basketballHoop.zPosition = -1
             //enemy.physicsBody?.collisionBitMask = PhysicsCategory.none
             addChild(basketballHoop)
             
             let actionMove = SKAction.pause()
                 basketballHoop.run(SKAction.sequence([actionMove]))
-         
-
-    
-
-}
+        }
     
         func didBegin(_ contact: SKPhysicsContact) {
             print("Add Score")
-            let one = contact.bodyA.node as? SKSpriteNode
-            let two = contact.bodyB.node as? SKSpriteNode
-            //one?.removeFromParent()
-            //two?.removeFromParent()
-       
+            scoreCount()
         }
-    
-
-   
- 
-    
-
-
-
-
-}
-
+    }
